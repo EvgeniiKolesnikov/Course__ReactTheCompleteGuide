831148caf5e76f12e73692f16275d4ede49d8060
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-15 22:17:58 +0300

    55. Updating State That Depends On The Previous State

--- src/components/NexExpense/ExpenseForm.js
+++ src/components/NexExpense/ExpenseForm.js
@@ -2,33 +2,39 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  })
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // })
 
   const titleChangeHandler = (e) => {
-    // setEnteredTitle(e.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: e.target.value});
+    setEnteredTitle(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: e.target.value});
+
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: e.target.value}
+    // })
   }
 
   const amountChangeHandler = e => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: e.target.value});
+    setEnteredAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: e.target.value});
   };
 
   const dateChangeHandler = e => {
-    setUserInput({
-      ...userInput,
-      enteredDate: e.target.value});
+    setEnteredDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: e.target.value});
   };
 
   return (
