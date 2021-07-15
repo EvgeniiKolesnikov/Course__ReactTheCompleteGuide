566ec2eedd17d194effdc942bf4ad1e2bd04ab41
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-15 23:25:53 +0300

    60. Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,23 +7,23 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // функция setTitle меняет динамически значение переменной title
   // и заново запускает jsx код
-  const [title, setTitle] = useState(props.title);
-  // let title = props.title;
-  console.log('ExpenseItem evaluated by React');
-  const clickHandler = () => {
-    // title = 'Updated';
-    setTitle('Updated');
-    console.log(title);
-  }
+  // const [title, setTitle] = useState(props.title);
+  // // let title = props.title;
+  // console.log('ExpenseItem evaluated by React');
+  // const clickHandler = () => {
+  //   // title = 'Updated';
+  //   setTitle('Updated');
+  //   console.log(title);
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
