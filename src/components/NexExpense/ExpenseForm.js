a2987106aadbdb502d4f270d4f00be2015b5b18c
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-15 22:04:47 +0300

    53. Working with Multiple States

--- src/components/NexExpense/ExpenseForm.js
+++ src/components/NexExpense/ExpenseForm.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredTitle(e.target.value);
   }
+
+  const amountChangeHandler = e => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandler = e => {
+    setEnteredDate(e.target.value);
+  };
 
   return (
     <form>
@@ -17,11 +28,11 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
         </div>
         
       </div>
