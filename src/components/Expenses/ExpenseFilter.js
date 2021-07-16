f4808eb234b3a7bc332728fff0adc43eba1bfbf6
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-16 16:54:23 +0300

    Task 3: Time to Practice: Working with Lists

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -15,7 +15,7 @@ const ExpenseFilter = (props) => {
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
-          <option value='2020'>2010</option>
+          <option value='2010'>2010</option>
         </select>
       </div>
     </div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,20 +12,27 @@ const Expenses = (props) => {
     setFilteredYaer(selectedYear);
   }
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter 
         selected={filteredYear} 
         onChangeFilter={filterChangeHandler} 
       />
-      {props.items.map(expense => 
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title} 
-          amount={expense.amount}
-          date={expense.date}
-        />
-      )}
+      {filteredExpenses.length === 0 ? 
+        (<p>No expenses found.</p>) :
+        (filteredExpenses.map(expense => 
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      }
     </Card>
   );
 }
