5afa83510c1e1f84fcd531a5023b85fc41c4346f
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-19 20:48:22 +0300

    124. Building & Using a Custom Context Provider Component

--- 10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ 10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -24,7 +25,8 @@ const passwordReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
+  const authCtx = useContext(AuthContext);
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -94,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- 10-side-effects-reducers-context-api/src/index.js
+++ 10-side-effects-reducers-context-api/src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+, document.getElementById('root'));
