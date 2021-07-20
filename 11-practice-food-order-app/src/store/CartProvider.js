c9ca1d29a6e6de2cd37ef148e876f8025ba1739e
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-21 02:03:42 +0300

    147. Making Items Removable

--- 11-practice-food-order-app/src/store/CartProvider.js
+++ 11-practice-food-order-app/src/store/CartProvider.js
@@ -14,7 +14,7 @@ const cartReducer = (state, action) => {
       (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
-    
+
     let updatedItems;
 
     if (existingCartItem) {
@@ -33,6 +33,28 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   }
+
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  
   return defaultCartState;
 };
 
