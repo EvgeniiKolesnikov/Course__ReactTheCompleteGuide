60985562d917bfa70b0790d23e120cba44821b42
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-21 23:32:03 +0300

    154. Preventing Unnecessary Re-Evaluations with React.memo()

--- 12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
+++ 12-a-look-behind-the-scenes/src/components/Demo/DemoOutput.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import MyParagraph from './MyParagraph';
+
 const DemoOutput = (props) => {
   console.log('DemoOutput RUNNING');
-  return <p>{props.show ? 'This is new!' : ''}</p>;
+  return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
