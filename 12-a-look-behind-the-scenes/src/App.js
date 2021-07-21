87e9bfa699c114ed1c5bfe7ea69799c67d67fed9
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-21 23:22:25 +0300

    153. A Closer Look At Child Component Re-Evaluation

--- 12-a-look-behind-the-scenes/src/App.js
+++ 12-a-look-behind-the-scenes/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false}/>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
