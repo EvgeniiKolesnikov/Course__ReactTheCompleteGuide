8586997640b700d9ca2a77222e9a59af2cef9bc1
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-24 13:46:35 +0300

    180. Preparing The Project For The Next Steps

--- 14-sending-http-requests/src/App.js
+++ 14-sending-http-requests/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -12,10 +13,10 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://react-http-4d8c5-default-rtdb.firebaseio.com/movies.json");
 
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -37,8 +38,12 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
-  
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
@@ -51,6 +56,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
