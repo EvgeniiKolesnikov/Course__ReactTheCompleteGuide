2868036e6fd2656ce1275dc8b52f36c50b77b62a
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-24 15:02:00 +0300

    189. Onwards To A More Realistic Example

---
+++ 15-building-custom-react-hooks/src/components/NewTask/NewTask.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const URL = 'https://react-http-4d8c5-default-rtdb.firebaseio.com/tasks.json';
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const enterTaskHandler = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        `${URL}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ text: taskText }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
