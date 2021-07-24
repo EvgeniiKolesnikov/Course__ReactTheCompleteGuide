f147e4b236e391dd7dcd4ab5201f819f675d041e
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-24 14:32:01 +0300

    187. Using Custom Hooks

--- 15-building-custom-react-hooks/src/App.js
+++ 15-building-custom-react-hooks/src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
-
   return (
     <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
   );
 }
