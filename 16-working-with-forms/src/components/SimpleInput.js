5bd8303e6de4b3671fdb00efdf0872fc123a9ead
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-25 14:41:32 +0300

    202. React To Lost Focus

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -15,9 +15,18 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+  
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    
+
     setEnteredNameTouched(true);
 
     if (enteredName.trim() === '') {
@@ -50,6 +59,7 @@ const SimpleInput = (props) => {
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
       {nameInputIsInvalid && (
