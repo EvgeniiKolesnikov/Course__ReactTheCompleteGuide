321917c6f3610b57a54b382466ab90f8ccc74e9d
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-27 15:35:05 +0300

    250. Refresher / Practice: Part 1/2

---
+++ 19-advanced-redux/src/store/cart-slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) { },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
