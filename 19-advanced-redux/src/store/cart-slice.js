11971a98942781a8557aba92674dd346876e2b0f
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-27 18:12:35 +0300

    259. Getting Started with Fetching Data

--- 19-advanced-redux/src/store/cart-slice.js
+++ 19-advanced-redux/src/store/cart-slice.js
@@ -43,50 +43,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        'https://react-http-4d8c5-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-    };
-
-    try {
-      await sendRequest();
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
