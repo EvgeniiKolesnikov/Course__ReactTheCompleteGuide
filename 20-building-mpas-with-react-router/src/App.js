b18d1c636368bff0d5b340aaa3c64928c43a1681
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-28 14:39:01 +0300

    276. Practice Redirecting & Extracting Params

--- 20-building-mpas-with-react-router/src/App.js
+++ 20-building-mpas-with-react-router/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
@@ -7,6 +7,9 @@ import NewQuote from './pages/NewQuote';
 function App() {
   return (
     <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
       <Route path='/quotes' exact>
         <AllQuotes />
       </Route>
--- 20-building-mpas-with-react-router/src/pages/QuoteDetail.js
+++ 20-building-mpas-with-react-router/src/pages/QuoteDetail.js
@@ -1,5 +1,20 @@
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
+
+import Comments from '../components/comments/Comments';
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
