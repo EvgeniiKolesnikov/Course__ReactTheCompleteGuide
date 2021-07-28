898b1ec13543735df415d8176a69e061f767c55f
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-28 14:36:06 +0300

    275. Time to Practice: Onwards to a New Project

--- 20-building-mpas-with-react-router/src/App.js
+++ 20-building-mpas-with-react-router/src/App.js
@@ -1,8 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ 20-building-mpas-with-react-router/src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuoteDetail = () => {
+  return <h1>Quote Detail Page</h1>
+};
+
+export default QuoteDetail;
