f9cfc27a42a059b54a12b7a5acf8ebcd55387988
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-28 13:58:41 +0300

    268. Working with Links

--- 20-building-mpas-with-react-router/src/App.js
+++ 20-building-mpas-with-react-router/src/App.js
@@ -2,16 +2,20 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
