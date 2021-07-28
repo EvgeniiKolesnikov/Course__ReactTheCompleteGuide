26e55afdc570dbd600028d95790c9f03207a48f7
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-28 14:04:32 +0300

    269. Using NavLinks

--- 20-building-mpas-with-react-router/src/components/MainHeader.js
+++ 20-building-mpas-with-react-router/src/components/MainHeader.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './MainHeader.module.css';
 
@@ -8,10 +8,14 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <Link to='/welcome'>Welcome</Link>
+            <NavLink activeClassName={classes.active} to='/welcome'>
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to='/products'>Products</Link>
+            <NavLink activeClassName={classes.active} to='/products'>
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
