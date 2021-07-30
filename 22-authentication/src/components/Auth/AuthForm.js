333c5699a5a812eb3751a30f65fd0e76d9ff3230
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-30 15:50:08 +0300

    302. Showing Feedback To The User

--- 22-authentication/src/components/Auth/AuthForm.js
+++ 22-authentication/src/components/Auth/AuthForm.js
@@ -8,6 +8,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -21,6 +22,7 @@ const AuthForm = () => {
 
     // optional: Add validation
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -37,12 +39,16 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
           // ...
         } else {
           return res.json().then((data) => {
-            // show an error modal
-            console.log(data);
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -67,7 +73,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type='button'
             className={classes.toggle}
