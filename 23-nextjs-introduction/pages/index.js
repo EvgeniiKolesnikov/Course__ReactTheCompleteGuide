55b53b2dea84268ba251850a0ad7aad50df14c87
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-02 23:58:47 +0300

    343. Adding "head" Metadata

--- 23-nextjs-introduction/pages/index.js
+++ 23-nextjs-introduction/pages/index.js
@@ -1,27 +1,23 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
 import { MongoClient } from 'mongodb';
 import MONGODB_LINK from './../secure/keys';
 
 import MeetupList from '../components/meetups/MeetupList';
 
-// const DUMMY_MEETUPS = [
-//   {
-//     id: 'm1',
-//     title: 'A First Meetup',
-//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-//     address: 'Some address 5, 12345 Some City',
-//     description: 'This is a first meetup!'
-//   },
-//   {
-//     id: 'm2',
-//     title: 'A Second Meetup',
-//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-//     address: 'Some address 10, 12345 Some City',
-//     description: 'This is a second meetup!'
-//   }
-// ];
-
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
