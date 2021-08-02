69473591eb625d71817d9f3cdf368cbc1a9d7187
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-02 23:20:27 +0300

    339. Working with MongoDB

--- 23-nextjs-introduction/pages/api/new-meetup.js
+++ 23-nextjs-introduction/pages/api/new-meetup.js
@@ -1,11 +1,28 @@
-// /api/new-meetup
+import { MongoClient } from 'mongodb';
+import { MONGODB_LINK } from './../../secure/keys';
 
-function handler(req, res) { 
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-  }
 
-  const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      `${MONGODB_LINK}`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
 }
 
 export default handler;
