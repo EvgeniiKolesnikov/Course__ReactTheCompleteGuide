73923192b5d41e98be9a90042948ab3c14d95df1
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-03 20:50:24 +0300

    354. Using the Transition Component

--- 24-animations/src/App.js
+++ 24-animations/src/App.js
@@ -48,9 +48,13 @@ class App extends Component {
             />
           )}
         </Transition>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
+        <Transition mountOnEnter
+          unmountOnExit in={this.state.modalIsOpen} timeout={300}>
+          {state => (
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
+
         {this.state.modalIsOpen ? (
           <Backdrop show={this.state.modalIsOpen} />
         ) : null}
