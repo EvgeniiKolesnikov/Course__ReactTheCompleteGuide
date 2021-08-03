69afab6eb82480f46b3ed399f0d404f3b2507cdc
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-03 21:00:31 +0300

    355. Wrapping the Transition Component

--- 24-animations/src/App.js
+++ 24-animations/src/App.js
@@ -48,16 +48,8 @@ class App extends Component {
             />
           )}
         </Transition>
-        <Transition mountOnEnter
-          unmountOnExit in={this.state.modalIsOpen} timeout={300}>
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
-
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
