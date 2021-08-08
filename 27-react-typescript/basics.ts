09d2bd9d64cc9ea15d0a9790dbaf7146e9079e0b
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-08 22:44:56 +0300

    401. Diving Into Generics

--- 27-react-typescript/basics.ts
+++ 27-react-typescript/basics.ts
@@ -43,3 +43,13 @@ function add(a: number, b: number) {
 function print(value: any) {
   console.log(value);
 }
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+// updatedArray[0].split('');
