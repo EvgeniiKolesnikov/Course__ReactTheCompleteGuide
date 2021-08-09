3d4b2286a4e994bd09ac91cb77f17e834d593165
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-09 22:24:42 +0300

    427. Sending Http Requests

--- 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
+++ 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -3,15 +3,25 @@ import React, { useState } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
-
+import { DB_LINK } from '../secure/keys';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch(`${DB_LINK}/ingredients.json`, {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
   };
 
   const removeIngredientHandler = ingredientId => {
