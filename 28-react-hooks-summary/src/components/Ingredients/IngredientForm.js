d91a8ba9632ad442bd5a0d6064fbbc899c5b7a66
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-09 23:17:40 +0300

    435. Loading Errors & State Batching

--- 28-react-hooks-summary/src/components/Ingredients/IngredientForm.js
+++ 28-react-hooks-summary/src/components/Ingredients/IngredientForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
-
+import LoadingIndicator from '../UI/LoadingIndicator';
 const IngredientForm = React.memo((props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
@@ -40,6 +40,7 @@ const IngredientForm = React.memo((props) => {
           </div>
           <div className='ingredient-form__actions'>
             <button type='submit'>Add Ingredient</button>
+            {props.loading && <LoadingIndicator />}
           </div>
         </form>
       </Card>
--- 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
+++ 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 import { DB_LINK } from '../secure/keys';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     fetch(`${DB_LINK}/ingredients.json`)
@@ -32,12 +35,14 @@ const Ingredients = () => {
   }, []);
   
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch(`${DB_LINK}/ingredients.json`, {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     })
     .then(response => {
+      setIsLoading(false);
       return response.json();
     })
     .then(responseData => {
@@ -49,18 +54,29 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true);
     fetch(`${DB_LINK}/ingredients/${ingredientId}.json`, {
       method: 'DELETE'
     }).then(response => {
+      setIsLoading(false);
       setUserIngredients(prevIngredients =>
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       );
+    }).catch(error => {
+      setError('Something went wrong!');
+      setIsLoading(false);
     });
   };
 
+  const clearError = () => {
+    setError(null);
+  }
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
