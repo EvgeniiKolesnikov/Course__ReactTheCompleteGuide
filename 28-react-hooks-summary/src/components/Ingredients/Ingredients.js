c2d4ff06e22b12d67ea3f56ea209f949f1d09d28
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-09 23:26:31 +0300

    437. Understanding useReducer()

--- 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
+++ 28-react-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -1,37 +1,53 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 import { DB_LINK } from '../secure/keys';
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there!');
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  useEffect(() => {
-    fetch(`${DB_LINK}/ingredients.json`)
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${DB_LINK}/ingredients.json`)
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount
+  //         });
+  //       }
+  //       setUserIngredients(loadedIngredients);
+  //     });
+  // }, []);
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
   
   const addIngredientHandler = ingredient => {
@@ -46,10 +62,14 @@ const Ingredients = () => {
       return response.json();
     })
     .then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient }
-      ]);
+      // setUserIngredients(prevIngredients => [
+      //   ...prevIngredients,
+      //   { id: responseData.name, ...ingredient }
+      // ]);
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: responseData.name, ...ingredient }
+      });
     });
   };
 
@@ -59,9 +79,10 @@ const Ingredients = () => {
       method: 'DELETE'
     }).then(response => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-      );
+      // setUserIngredients(prevIngredients =>
+      //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+      // );
+      dispatch({ type: 'DELETE', id: ingredientId });
     }).catch(error => {
       setError('Something went wrong!');
       setIsLoading(false);
