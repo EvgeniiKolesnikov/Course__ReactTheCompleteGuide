bfa2bc6d244602f1188fdef02048200a4eba393e
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-09 21:39:08 +0300

    421 - 423. useState()

--- 28-react-hooks-summary/src/components/Ingredients/IngredientForm.js
+++ 28-react-hooks-summary/src/components/Ingredients/IngredientForm.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
@@ -15,11 +17,33 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState.title}
+              onChange={(event) => {
+                const newTitle = event.target.value;
+                setInputState((prevInputState) => ({
+                  title: newTitle,
+                  amount: prevInputState.amount,
+                }));
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={(event) => {
+                const newAmount = event.target.value;
+                setInputState((prevInputState) => ({
+                  amount: newAmount,
+                  title: prevInputState.title,
+                }));
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
