381b50673e95cded426129fa05638d2ab9678c76
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-12 23:16:51 +0300

    473. Getting Started with Fetching Data

--- 29-react-summary/src/pages/AllMeetups.js
+++ 29-react-summary/src/pages/AllMeetups.js
@@ -1,31 +1,56 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { useState } from 'react';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+import MeetupList from '../components/meetups/MeetupList';
+import { DB_LINK } from '../secure/keys';
+
+// const DUMMY_DATA = [
+//   {
+//     id: 'm1',
+//     title: 'This is a first meetup',
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+//   {
+//     id: 'm2',
+//     title: 'This is a second meetup',
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+// ];
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  fetch(`${DB_LINK}/meetups.json`)
+  .then(response => {
+    return response.json();
+  })
+  .then((data) => {
+    const meetups = [];
+
+    setIsLoading(false);
+    setLoadedMeetups(meetups);
+  });
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
       {/* <ul>
         {DUMMY_DATA.map((meetup) => {
           return <li key={meetup.id}>{meetup.title}</li>;
