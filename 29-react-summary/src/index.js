52d5236eb6b03e79f614daec071a4ae46daa3bdb
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-12 23:49:23 +0300

    475 - 478. React Context

--- 29-react-summary/src/index.js
+++ 29-react-summary/src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <App />
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+);
